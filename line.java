b2ab4c5149dc68e280a3a39e1b241d9feeec5c99
saitarun135 <saitarun135@gm.com>
2021-03-09 19:36:08 +0530

    lengths of a line

--- line.java
+++ line.java
@@ -1,7 +1,31 @@
-public class line {
-		
-	public static void main(String args[]) {
-	System.out.println("WELCOME TO THE LINE PROBLEM");
+import java.util.Scanner;
 
+	public class line {
+		
+			public static void main(String args[]) {
+	 			int x1,y1,x2,y2;
+	   		int a1,b1,a2,b2;
+				Scanner sc =new Scanner(System.in);
+	   			System.out.println("Enter x1 value :");
+	  				 x1=sc.nextInt();
+	   			System.out.println("Enter x2 value :");
+	   				x2=sc.nextInt();
+	   			System.out.println("Enter y1 value :");
+	   				y1=sc.nextInt();
+	   			System.out.println("Enter y2 value :");
+	   				y2=sc.nextInt();
+	       		System.out.println("Enter a1 value :");
+	         		a1=sc.nextInt();
+	      		System.out.println("Enter a2 value :");
+	         		a2=sc.nextInt();
+	      		System.out.println("Enter b1 value :");
+	         		b1=sc.nextInt();
+	      		System.out.println("Enter b2 value :");
+	         		b2=sc.nextInt();
+
+		Double line1=Math.sqrt((x2-x1)^2+(y2-y1)^2);
+		Double line2=Math.sqrt((a2-a1)^2+(b2-b1)^2);
+		System.out.println(line1);
+		System.out.println(line2);
 }
 }
